b2d84ea00e5b947a75ff72fcc53e8c648d93cc4f
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-17 15:47:35 +0330

    Add PaymentHistory model to track user payment transactions

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from subscriptions.models import Subscription
 from django.contrib.auth.models import User
 # Create your models here.
 
 class Wallet(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='wallet')
     balance = models.IntegerField()
+
+class PaymentHistory(models.Model):
+    PAYMENT_STATUS = [
+        ('SUCCESS','Success'),
+        ('FAILED','Failed'),
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='payment_history')
+    subscription = models.ForeignKey(Subscription,on_delete=models.CASCADE,related_name='payment_history')
+    wallet = models.ForeignKey(Subscription,on_delete=models.CASCADE,related_name='payment_history')
+    amount = models.IntegerField()
+    status = models.CharField(max_length=30,choices=PAYMENT_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)    
