30d35a0b24d51f95b10105fee5c68c08e37d7682
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-17 11:22:29 +0330

    Add subscription models with SubscriptionType and Subscription classes

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class SubscriptionType(models.Model):
+    SUB_TYPE = [
+        ('FREE','Free'),
+        ('BRONZE','Bronze'),
+        ('SILVER','Silver'),
+        ('GOLD','Gold'),
+    ]
+    type = models.CharField(max_length=20,choices=SUB_TYPE)
+    price = models.FloatField()
+    duration = models.DurationField()
+
+class Subscription(models.Model):
+    SUB_STATUS = [
+        ('EXPIRED','Expired'),
+        ('CANCELLED','Cancelled'),
+        ('ACTIVE','Active'),
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='subscriptions')
+    subscriptionType = models.ForeignKey(SubscriptionType,on_delete=models.CASCADE,related_name='subscriptions')
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=30,choices=SUB_STATUS)
