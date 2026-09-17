cc7a69c7958009670140a013ee6f76e1cc3b082b
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-17 17:40:49 +0330

    Register CustomUserAdmin for User model with specified list display fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, AdminUser
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
