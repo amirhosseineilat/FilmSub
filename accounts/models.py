4669457e10e114f2928085a5fb5383ebc18c6fa0
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-17 16:08:59 +0330

    Add AdminUser model with staff and superuser permissions

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    is_staff = False
+
+class AdminUser(AbstractUser):
+    is_staff = True
+    is_superuser = True
