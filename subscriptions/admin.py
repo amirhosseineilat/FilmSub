529203d97e3c243d7085bd8ff368eb592d0ede70
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-17 17:03:15 +0330

    Refactor admin registration for Subscription and SubscriptionType models

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import SubscriptionType,Subscription
 
 # Register your models here.
+
+@admin.register(Subscription)
+class SubscriptionTypeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'subscriptionType',
+        'start_time',
+        'end_time',
+        'status',
+    )
+
+    list_filter = (
+        "start_time",
+        "end_time",
+        "status",
+    )
+
+    search_fields = (
+        'user__username',
+        'user__email',
+    )
+
+    list_per_page = 50
+
+@admin.register(SubscriptionType)
+class SubscriptionTypeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'type',
+        'price',
+        'duration',
+
+    )
+
+    search_fields = (
+        'type',
+        'price',
+        'duration',
+        
+    )
+
+    list_filter = (
+        "type",
+    )
+
+    list_per_page = 50
