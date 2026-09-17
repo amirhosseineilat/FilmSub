5ecde83adf7f5fe15bd89618c01a03b750d5bacb
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-17 16:01:48 +0330

    Refactor user model imports to use get_user_model() for better compatibility

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from subscriptions.models import Subscription
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Wallet(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='wallet')
@@ -14,7 +15,7 @@ class PaymentHistory(models.Model):
     ]
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='payment_history')
     subscription = models.ForeignKey(Subscription,on_delete=models.CASCADE,related_name='payment_history')
-    wallet = models.ForeignKey(Subscription,on_delete=models.CASCADE,related_name='payment_history')
+    wallet = models.ForeignKey(Wallet,on_delete=models.CASCADE,related_name='payment_history')
     amount = models.IntegerField()
     status = models.CharField(max_length=30,choices=PAYMENT_STATUS)
     created_at = models.DateTimeField(auto_now_add=True)    
--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 from subscriptions.models import SubscriptionType
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
