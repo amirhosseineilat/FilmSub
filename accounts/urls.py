b32ade699a841f9d018ded79e3455c262d08117b
amirhosseineilat <amirhosseineilat@gmail.com>
2026-09-19 19:25:37 +0330

    Add UserActive view and update URL routing for user authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import Register
+from .views import Register,UserActive
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path("register/",Register.as_view()),
     path("login/",TokenObtainPairView.as_view()),
     path("token/refresh/",TokenRefreshView.as_view()),
+    path("me/",UserActive.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView 
+from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response 
+from rest_framework import status
 
 # Create your views here.
 
@@ -10,4 +14,15 @@ User = get_user_model()
 class Register(CreateAPIView):
     model = User
     serializer_class = RegisterSerializer
+
+class UserActive(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request,*args,**kwargs):
+        data = {
+            "username":request.user.username,
+            "is_authenticated":request.user.is_authenticated
+        }
+        return Response(data,status=status.HTTP_200_OK)
 
